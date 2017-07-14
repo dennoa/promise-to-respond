71ddff50db661371c5f9d73cea5fccd5089065e8
denno <andrew.dennison@live.com.au>
2017-07-14 17:32:48 +1000

    minor refactor

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,7 +6,7 @@ const expect = require('chai').expect;
 const sinon = require('sinon');
 
 describe('respond', ()=> {
-  var respondInstance, res, resStatus, resSend, resJson;
+  let respondInstance, res, resStatus, resSend, resJson;
 
   beforeEach(()=> {
     respondInstance = respond();
@@ -17,9 +17,7 @@ describe('respond', ()=> {
   });
 
   it('should respond with 204 on success with no content', (done)=> {
-    respondInstance(res, new Promise((resolve, reject)=> {
-      resolve();
-    })).then(()=> {
+    respondInstance(res, Promise.resolve()).then(()=> {
       expect(resStatus.calledWith(204)).to.be.true;
       expect(resSend.calledOnce).to.be.true;
       done();
@@ -27,10 +25,8 @@ describe('respond', ()=> {
   });
 
   it('should respond with 200 on success with content', (done)=> {
-    let json = { result: 'my result' };
-    respondInstance(res, new Promise((resolve, reject)=> {
-      resolve(json);
-    })).then(()=> {
+    const json = { result: 'my result' };
+    respondInstance(res, Promise.resolve(json)).then(()=> {
       expect(resStatus.calledWith(200)).to.be.true;
       expect(resJson.calledWith(json)).to.be.true;
       done();
@@ -38,10 +34,8 @@ describe('respond', ()=> {
   });
 
   it('should respond with 400 where an array of errors was supplied', (done)=> {
-    let errors = [{ param: 'myField', msg: 'required' }];
-    respondInstance(res, new Promise((resolve, reject)=> {
-      reject(errors);
-    })).then(()=> {
+    const errors = [{ param: 'myField', msg: 'required' }];
+    respondInstance(res, Promise.reject(errors)).then(()=> {
       expect(resStatus.calledWith(400)).to.be.true;
       expect(resJson.calledWith(errors)).to.be.true;
       done();
@@ -49,21 +43,17 @@ describe('respond', ()=> {
   });
 
   it('should respond with 404 where no error was supplied', (done)=> {
-    respondInstance(res, new Promise((resolve, reject)=> {
-      reject();
-    })).then(()=> {
+    respondInstance(res, Promise.reject()).then(()=> {
       expect(resStatus.calledWith(404)).to.be.true;
       done();
     });
   });
 
   it('should respond with 500 where some other kind of errors were supplied', (done)=> {
-    let errors = 'crap';
-    respondInstance(res, new Promise((resolve, reject)=> {
-      reject(errors);
-    })).then(()=> {
+    const errors = 'crap';
+    respondInstance(res, Promise.reject(errors)).then(()=> {
       expect(resStatus.calledWith(500)).to.be.true;
-      let msg = resJson.firstCall.args[0];
+      const msg = resJson.firstCall.args[0];
       expect(msg.error).to.equal(errors);
       done();
     });
@@ -73,10 +63,10 @@ describe('respond', ()=> {
     respondInstance = respond({
       sanitizer: json => _.omit(json, '_id')
     });
-    let json = { _id: 'sdfdsf', result: 'my result' };
-    respondInstance(res, new Promise(resolve => resolve(json))).then(()=> {
+    const json = { _id: 'sdfdsf', result: 'my result' };
+    respondInstance(res, Promise.resolve(json)).then(()=> {
       expect(resStatus.calledWith(200)).to.be.true;
-      let result = resJson.firstCall.args[0];
+      const result = resJson.firstCall.args[0];
       expect(result.result).to.equal(json.result);
       expect(typeof result._id).to.equal('undefined');
       done();
@@ -87,10 +77,10 @@ describe('respond', ()=> {
     respondInstance = respond({
       fieldsToOmit: '_id'
     });
-    let json = { _id: 'sdfdsf', result: 'my result' };
-    respondInstance(res, new Promise(resolve => resolve(json))).then(()=> {
+    const json = { _id: 'sdfdsf', result: 'my result' };
+    respondInstance(res, Promise.resolve(json)).then(()=> {
       expect(resStatus.calledWith(200)).to.be.true;
-      let result = resJson.firstCall.args[0];
+      const result = resJson.firstCall.args[0];
       expect(result.result).to.equal(json.result);
       expect(typeof result._id).to.equal('undefined');
       done();
@@ -101,10 +91,10 @@ describe('respond', ()=> {
     respondInstance = respond({
       fieldsToOmit: ['_id', '__v']
     });
-    let json = { _id: 'sdfdsf', __v: 0, result: 'my result' };
-    respondInstance(res, new Promise(resolve => resolve(json))).then(()=> {
+    const json = { _id: 'sdfdsf', __v: 0, result: 'my result' };
+    respondInstance(res, Promise.resolve(json)).then(()=> {
       expect(resStatus.calledWith(200)).to.be.true;
-      let result = resJson.firstCall.args[0];
+      const result = resJson.firstCall.args[0];
       expect(result.result).to.equal(json.result);
       expect(typeof result._id).to.equal('undefined');
       expect(typeof result.__v).to.equal('undefined');
@@ -116,10 +106,10 @@ describe('respond', ()=> {
     respondInstance = respond({
       fieldsToOmit: ['_id', '__v']
     });
-    let docs = [{ _id: 'sdfdsf', __v: 0, result: 'my result' },{ _id: 'sdfdsf', __v: 0, result: 'my other result' }];
-    respondInstance(res, new Promise(resolve => resolve(docs))).then(()=> {
+    const docs = [{ _id: 'sdfdsf', __v: 0, result: 'my result' },{ _id: 'sdfdsf', __v: 0, result: 'my other result' }];
+    respondInstance(res, Promise.resolve(docs)).then(()=> {
       expect(resStatus.calledWith(200)).to.be.true;
-      let result = resJson.firstCall.args[0];
+      const result = resJson.firstCall.args[0];
       docs.forEach((json, idx) => {
         expect(result[idx].result).to.equal(json.result);
         expect(typeof result[idx]._id).to.equal('undefined');
@@ -133,10 +123,10 @@ describe('respond', ()=> {
     respondInstance = respond({
       fieldsToOmit: ['myarr._id', 'myarr.__v']
     });
-    let docs = { myarr: [{ _id: 'sdfdsf', __v: 0, result: 'my result' },{ _id: 'sdfdsf', __v: 0, result: 'my other result' }] };
-    respondInstance(res, new Promise(resolve => resolve(docs))).then(()=> {
+    const docs = { myarr: [{ _id: 'sdfdsf', __v: 0, result: 'my result' },{ _id: 'sdfdsf', __v: 0, result: 'my other result' }] };
+    respondInstance(res, Promise.resolve(docs)).then(()=> {
       expect(resStatus.calledWith(200)).to.be.true;
-      let result = resJson.firstCall.args[0];
+      const result = resJson.firstCall.args[0];
       docs.myarr.forEach((json, idx) => {
         expect(result.myarr[idx].result).to.equal(json.result);
         expect(typeof result.myarr[idx]._id).to.equal('undefined');
@@ -150,10 +140,10 @@ describe('respond', ()=> {
     respondInstance = respond({
       fieldsToOmit: ['myarr.nested.arr.ignore']
     });
-    let docs = { myarr: [{ someDate: new Date(), nested: { arr: [{ ignore: 'me', keep: true }]}, result: 'my result' }] };
-    respondInstance(res, new Promise(resolve => resolve(docs))).then(()=> {
+    const docs = { myarr: [{ someDate: new Date(), nested: { arr: [{ ignore: 'me', keep: true }]}, result: 'my result' }] };
+    respondInstance(res, Promise.resolve(docs)).then(()=> {
       expect(resStatus.calledWith(200)).to.be.true;
-      let result = resJson.firstCall.args[0];
+      const result = resJson.firstCall.args[0];
       expect(result.myarr[0].result).to.equal(docs.myarr[0].result);
       expect(result.myarr[0].someDate.getTime()).to.equal(docs.myarr[0].someDate.getTime());
       expect(typeof result.myarr[0].nested.arr[0].ignore).to.equal('undefined');
@@ -166,10 +156,10 @@ describe('respond', ()=> {
     respondInstance = respond({
       fieldsToOmit: ['myarr.nested.arr.ignore']
     });
-    let docs = { myarr: [{ someDate: new Date(), nested: { arr: null }, result: 'my result' }] };
-    respondInstance(res, new Promise(resolve => resolve(docs))).then(()=> {
+    const docs = { myarr: [{ someDate: new Date(), nested: { arr: null }, result: 'my result' }] };
+    respondInstance(res, Promise.resolve(docs)).then(()=> {
       expect(resStatus.calledWith(200)).to.be.true;
-      let result = resJson.firstCall.args[0];
+      const result = resJson.firstCall.args[0];
       expect(result.myarr[0].nested.arr).to.equal(null);
       done();
     });
@@ -180,8 +170,8 @@ describe('respond', ()=> {
       sanitizer: docs => _.filter(docs, doc => !!doc._id),
       fieldsToOmit: ['_id', '__v']
     });
-    let docs = [{ _id: 'sdfdsf', __v: 0, result: 'my result' },{ _id: 'sdfdsf', __v: 0, result: 'my other result' }];
-    respondInstance(res, new Promise(resolve => resolve(docs))).then(()=> {
+    const docs = [{ _id: 'sdfdsf', __v: 0, result: 'my result' },{ _id: 'sdfdsf', __v: 0, result: 'my other result' }];
+    respondInstance(res, Promise.resolve(docs)).then(()=> {
       expect(resStatus.calledWith(200)).to.be.true;
       expect(resJson.calledWith([])).to.equal(true);
       done();
